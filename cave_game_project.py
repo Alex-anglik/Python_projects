92a03d069c75105aefd05f83b4a8892ba3409779
Alex-anglik <alexandersharman009@gmail.com>
2022-05-17 17:57:57 +0200

    fixed cave game

--- cave_game_project.py
+++ cave_game_project.py
@@ -1,14 +1,6 @@
 import random
-import os 
+import os
 
-
-#blank_map=[]
-#lines=[]
-#for i in range(10):
-#    lines.append(0)
-#for x in range(10):
- #   blank_map.append(lines)
-#print(blank_map)
 
 def map_symbols(symbol):
     if symbol == 0:
@@ -24,6 +16,7 @@ def map_symbols(symbol):
     
 
 def display_map(a_map):
+    os.system('clear')
     print('+' + '-'*10 + '+')
     for line in a_map:
         print('|' + ''.join(map_symbols(symbol) for symbol in line) + '|')
@@ -34,8 +27,8 @@ base_map = [[0 for _ in range(10)] for _ in range(10)]
 
 def generate_map():
     
-    print("Base map:")
-    display_map(base_map)
+    #print("Base map:")
+    #display_map(base_map)
     
     for _ in range(10):
         obstacle_x = random.randint(0,9)
@@ -68,7 +61,7 @@ def generate_map():
         t_y = random.randint(0,9)
         treasure_positioned = (abs(player_x - t_x) + abs(player_y - t_y) > 4) and (base_map[t_x][t_y] != -1) and (abs(monster_x - t_x) + abs(monster_y - t_y) > 4)
     base_map[t_x][t_y] = 3
-    print("completed map:")
+   # print("completed map:")
     display_map(base_map)
 
     return base_map
@@ -143,13 +136,6 @@ def move_right():
         base_map[player_x][player_y]=0
         player_y += 1
         base_map[player_x][player_y]=1
-        #print(player_x)
-        #print(player_y)
-        #if base_map[t_x][t_y]==1:
-        #    print("you win!!!")
-         #   return False
-        #else:
-        #    return True
     else:
         print("you cant move here, try again ")
     return 
@@ -161,11 +147,7 @@ def move_left():
         base_map[player_x][player_y]=0
         player_y -= 1
         base_map[player_x][player_y]=1
-        #if base_map[t_x][t_y]==1:
-        #    print("you win!!!")
-        #    return False
-        #else:
-         #   return True
+       
     else:
         print("you cant move here, try again ")
     return 
@@ -177,11 +159,7 @@ def move_down():
         base_map[player_x][player_y]=0
         player_x += 1
         base_map[player_x][player_y]=1
-        #if base_map[t_x][t_y]==1:
-         #   print("you win!!!")
-         #  return False
-        #else:
-         #  return True
+       
     else:
         print("you cant move here, try again ")
     return 
@@ -193,30 +171,17 @@ def move_up():
         base_map[player_x][player_y]=0
         player_x -= 1
         base_map[player_x][player_y]=1
-        #if base_map[t_x][t_y]==1:
-          #  print("you win!!!")
-            #return False
-        #else:
-         #   return True 
+        
     else:
         print("you cant move here, try again ")
     return 
 
-#def outcome():
- #   global t_x, t_y
-#    if base_map[t_x][t_y]==1:
-        
-  #      return False
-    #elif base_map[player_x][player_y]==3:
-     #   print("You win!!! good job")
-     #   return False
- #   else:
-  #      return True
+
 
 def find_treasure():
     for k in range(10):
         for l in range(10):
-            if base_map[k][l]==1:
+            if base_map[k][l]==3:
                 
                 return k, l
 
@@ -230,37 +195,47 @@ def moving():
         move=input("where would you like to move?(w,a,s,d)")
         
         if move=="a":
-            move_left()
-            #temp=outcome()
-            #if temp==False:
-            #    print("you win!!!")
-           #     again=False
+            #move_left()
+            
+            if base_map[player_x][player_y-1]==3:
+                print("you win, good job!")
+                again=False
+                return
+            else:
+                move_left()
 
         elif move=="d":
-            move_right()
-            #temp=outcome()
-            #if temp==False:
-            #    print("you win!!!")
-           #    again=False
+            #move_right()
+            print("move d")
+            if base_map[player_x][player_y+1]==3:
+                print("you win, good job!")
+                again=False
+                return
+            else:
+                move_right()
+
         elif move=="w":
-            move_up()
-           # temp=outcome()
-           # if temp==False:
-           #     print("you win!!!")
-           #     again=False
+            
+            if base_map[player_x-1][player_y]==3:
+                print("you win, good job!")
+                again=False
+                return
+            else:
+                move_up()
         elif move=="s":
-            move_down()
-            #temp=outcome()
-            #if temp==False:
-           #     print("you win!!!")
-            #    again=False
+            #move_down()
+            if base_map[player_x+1][player_y]==3:
+                print("you win, good job!")
+                again=False
+                return
+            else:
+                move_down()
         else:
             print("wrong input")
         
             
 
         display_map(base_map)
-        
     return 
 
 
